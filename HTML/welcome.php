b4fd6e8748ed02061a177f37d8d1631dadc57596
renzotav <renzotavaresmkn@gmail.com>
2024-10-09 09:58:00 -0300

    Atualizando pasta

---
+++ HTML/welcome.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+    Welcome <?php echo $_POST["nome"]; ?>
+    seu email: <?php echo $_POST["email"]; ?>
+</body>
+</html>
